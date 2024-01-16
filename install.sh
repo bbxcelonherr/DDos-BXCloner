2ece2f437c7245ab2dbb80a4c47ffddc4c3ac29d
matildes <matildes@student.21-school.ru>
2024-01-16 09:40:38 +0500

    B-XCloner Tool for DDos

---
+++ install.sh
@@ -0,0 +1,20 @@
+clear
+echo """
+		 ____________________________________________
+		|					                         |
+		|          The Code By :- B-XCloner UZB      |
+		|	       telegram channel :- @bxcloner     |
+		+--------------------------------------------+
+		|                                            |
+		|       Git Hub :- github.com/bbxcelonherr   |
+		|____________________________________________| 
+		|                                            |
+		|    Dunyoning asl haqiqati - sening bu      |
+		|       haqiqatni bilmasligingdadir:)        |
+		|                                            |
+		|                    By B-XCloner            |
+		|____________________________________________|
+		"""
+pkg install figlet -y 
+cp -a DDos-BXCloner.py /data/data/com.termux/files/usr/bin
+echo "run [DDos-BXCloner.py] in now tabe "
